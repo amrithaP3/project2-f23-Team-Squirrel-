594aa969e732692243f020c27c03ee6b5d6ff75f
shanmathig <shanmathi7215@gmail.com>
2023-11-16 19:43:55 -0500

    add verify.js

--- server/mongodb/actions/readTraining.js
+++ server/mongodb/actions/readTraining.js
@@ -1,16 +1,23 @@
-import connectDB from '../index.js'
-import Training from '../models/Training.js'
-import Animal from '../models/Animal.js'
-import User from '../models/User.js'
+import connectDB from '../index.js';
+import Training from '../models/Training.js';
+import Animal from '../models/Animal.js';
+import User from '../models/User.js';
 
 export default async function readTraining(data) {
-    try {
-        await connectDB()
-        const { id } = data
-        if (User.exists({_id: id}) === null) return false
-        return await Training.find({ "user": id })
-    } 
-    catch (e) {
-        console.log(e)
+  try {
+    await connectDB();
+    const { id } = data;
+    const userExists = await User.exists({ _id: id });
+    
+    if (!userExists) {
+      return false;
     }
+    const trainingData = await Training.find({ user: id });
+    const animalData = await Animal.find({ owner: id });
+
+    return { trainingData, animalData };
+  } catch (e) {
+    console.error('Error reading training data:', e);
+    throw e;
+  }
 }
---
+++ src/pages/api/user/verify.js
@@ -0,0 +1,28 @@
+
+import connectDB from '../../../../index.js'; 
+import User from '../../../../models/User.js'; 
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  try {
+    await connectDB();
+    
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && user.comparePassword(password)) {
+      return res.status(200).json({
+        userId: user._id,
+        isAdmin: user.isAdmin || false,
+      });
+    } else {
+      return res.status(500).json({ error: 'Invalid email or password' });
+    }
+  } catch (error) {
+    console.error('Error in verify API:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
