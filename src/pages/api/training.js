1a0f5ca590567a6eb7abb8b717e86d854cfc7f8a
Daniel Ayoung-Chee <dayoungchee3@gatech.edu>
2023-11-16 23:30:40 -0500

    Endpoints finalized

--- src/pages/api/training.js
+++ src/pages/api/training.js
@@ -1,7 +1,7 @@
 import createTraining from "../../../server/mongodb/actions/createTraining.js"
 
 export default async function handler(req, res) {
-    if (req.method == 'POST') {
+    if (req.method === 'POST') {
         try {
             await createTraining(req.body)
         } catch (e) {
--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,7 +1,7 @@
 import createUser from "../../../server/mongodb/actions/createUser.js"
 
 export default async function handler(req, res) {
-    if (req.method == 'POST') {
+    if (req.method === 'POST') {
         try {
             await createUser(req.body)
         } catch (e) {
