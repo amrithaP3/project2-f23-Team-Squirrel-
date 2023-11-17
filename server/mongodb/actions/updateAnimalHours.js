be1b2aabd42d9c840711fb3c33be034479b5354b
Daniel Ayoung-Chee <dayoungchee3@gatech.edu>
2023-11-16 19:37:48 -0500

    did readUsers

--- server/mongodb/actions/updateAnimalHours.js
+++ server/mongodb/actions/updateAnimalHours.js
@@ -2,7 +2,7 @@ import Animal from '../models/Animal.js'
 
 export default async function updateAnimalHours(data) {
     try {
-        await Animal.findByIdAndUpdate(data.animalID, { 'hoursTrained': data.hours })
+        await Animal.findByIdAndUpdate(data.animalId, { 'hoursTrained': data.hours })
     } 
     catch (e) {
         console.log(e)
