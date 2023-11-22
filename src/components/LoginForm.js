20b1c6006a19d3764bb6a8ee911391c1426796f0
shanmathig <shanmathi7215@gmail.com>
2023-11-22 14:44:30 -0500

    passwords don't match

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,37 +3,66 @@ import React from 'react';
 class LoginForm extends React.Component {
 
     state = {
-        email: " ",
-        paassword: " "
+        fullname: "",
+        email: "",
+        password: "",
+        confirmpass: "",
+        error: ""
     };
 
     handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value});
+        this.setState({ [event.target.name]: event.target.value, error: "" });
     }
 
     handleSubmit = event => {
         event.preventDefault();
-        console.log(this.state);
+
+        const { password, confirmpass } = this.state;
+
+        if (password !== confirmpass) {
+            this.setState({ error: "Passwords do not match" });
+        } else {
+            console.log(this.state);
+        }
     };
 
     render() {
         return (
-            <form onSubmit = {this.handleSubmit}>
+            <form onSubmit={this.handleSubmit}>
                 <div>
-                    <input type = "email"
-                    name = "email"
-                    placeholder = "email"
-                    onChange = {this.handleChange}
+                    <input type="text"
+                        name="fullname"
+                        placeholder="Full Name"
+                        onChange={this.handleChange}
+                        value={this.state.fullname}
                     />
                 </div>
                 <div>
-                    <input type = "password"
-                    name = "password"
-                    placeholder = "password"
-                    onChange = {this.handleChange}
+                    <input type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={this.handleChange}
+                        value={this.state.email}
                     />
                 </div>
-                <button type = "submit">Submit</button>
+                <div>
+                    <input type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={this.handleChange}
+                        value={this.state.password}
+                    />
+                </div>
+                <div>
+                    <input type="password"
+                        name="confirmpass"
+                        placeholder="Confirm Password"
+                        onChange={this.handleChange}
+                        value={this.state.confirmpass}
+                    />
+                </div>
+                {this.state.error && <div style={{ color: 'red' }}>{this.state.error}</div>}
+                <button type="submit">Sign Up</button>
             </form>
         )
     }
