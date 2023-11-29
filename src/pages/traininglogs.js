2c60f2a6924098c8b4fcbe34205a6316a87b4005
Daniel Ayoung-Chee <dayoungchee3@gatech.edu>
2023-11-29 13:56:28 -0500

    merge conflicts

--- src/pages/traininglogs.js
+++ src/pages/traininglogs.js
@@ -6,7 +6,7 @@ import AnimalComponent from '../components/AnimalComponent.js';
 
 export default function traininglogs() {
     const { userId } = useAuth();
-    const [ animals, setAnimals ] = useState(null)
+    const [animals, setAnimals] = useState(null);
     const router = useRouter();
 
     useEffect(() => {
@@ -16,21 +16,37 @@ export default function traininglogs() {
     }, [userId]);
 
     useEffect(() => {
-        async function data() {
-            const res = await fetch("/api/admin/animals")
-            const data = await res.json()
-            setAnimals(data);
-    },[userId]);
-    console.log("userid is this:" + userId);
+        async function fetchAnimals() {
+            try {
+                const res = await fetch("/api/admin/animals");
+                if (!res.ok) {
+                    throw new Error('Failed to fetch animals');
+                }
+                const data = await res.json();
+                console.log('Fetched animals:', data); // Check the fetched data
+                setAnimals(data);
+            } catch (error) {
+                console.error('Error fetching animals:', error);
+            }
+        }
+
+        if (userId !== -1) { // Fetch animals only if the user ID is valid
+            fetchAnimals();
+        }
+    }, [userId]);
+
     return (
         <>
             <h1>TrainingLogs dashboard</h1>
             <TrainingLogs />
+            {animals ? animals.map((animal) => (
+                <AnimalComponent animal={animal} key={animal._id} />
+            )) : <p>Loading animals...</p>}
             {/* display search bar */}
             {/* display side bar */}
             {/* display top portion of list */}
             {/* display list components filtered by userId */}
         </> 
-    )
+    );
 }
 
