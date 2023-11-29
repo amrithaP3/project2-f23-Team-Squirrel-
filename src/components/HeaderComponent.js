19e7a2c722b4038de64e1f891041a22372b868dd
Daniel Ayoung-Chee <dayoungchee3@gatech.edu>
2023-11-29 14:30:25 -0500

    header adjustments

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,15 +1,13 @@
-// components/HeaderComponent.js
-
 import React from 'react';
 import Image from 'next/image';
-import styles from '../styles/style.module.css'; // Adjust the path if necessary
+import styles from '../styles/style.module.css';
 
 const HeaderComponent = () => {
   return (
     <div className={styles['page-header']}>
       <div className={styles['header-content']}>
         <Image src="/applogo.png" alt="App Logo" width={100} height={100} className={styles.appLogo} />
-        <div className={styles.heading1}>Progress</div>
+        <div className={styles['heading-words']}>Progress</div>
       </div>
       <div className={styles['gray-line']}></div>
     </div>
