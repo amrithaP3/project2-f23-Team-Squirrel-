f6cfa1d7f829c6fb7ee26ae998e8deb355338674
Daniel Ayoung-Chee <dayoungchee3@gatech.edu>
2023-11-30 15:52:40 -0500

    Amritha- training log component styling

--- src/components/TrainingLog.js
+++ src/components/TrainingLog.js
@@ -1,39 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link'; 
 import Image from 'next/image';
-import styles from '@/styles/TrainingLog.module.css';
+import styles from '../../src/styles/TrainingDashboard.module.css';
+import Animal from '../../server/mongodb/models/Animal';
+import connectDB from '../../server/mongodb';
 import { useAuth } from '@/hooks/useAuth';
 
 export default function TrainingLog(props) {
 
     const { userId, admin, fullName, login, logout } = useAuth();
-    const { user, animal, title, date, description, hours } = props;
-
-    //const [userData, setUserData] = useState(null);
-
-    /**
-    useEffect(() => {
-        const fetchUserData = async () => {
-        try {
-            // Fetch user data from the backend
-            const response = await fetch(`/api/user/${userId}`);
-            const data = await response.json();
-
-            // Set the user data in state
-            setUserData(data);
-        } catch (error) {
-            console.error('Error fetching user data:', error);
-        }
-        };
-
-        // Call the fetchUserData function
-        fetchUserData();
-    }, [userId]);
-
-    */
-
-    //const user = await User.findById(userId);
-    //const name = user.fullname;
+    const { user, title, animal, date, description, hours } = props;
 
     function formatDateComponents(dateString) {
         // Create a Date object from the string
@@ -49,22 +25,88 @@ export default function TrainingLog(props) {
 
     // works for Format: YYYY-MM-DD
     const components = formatDateComponents(date);
+    
+    let month;
+    switch (components.month) {
+        case 1:
+            month = 'Jan';
+        case 2:
+            month = 'Feb';
+        case 3:
+            month = 'Mar';
+        case 4:
+            month = 'Apr';
+        case 5:
+            month = 'May';
+        case 6:
+            month = 'Jun';
+        case 7:
+            month = 'Jul';
+        case 8:
+            month = 'Aug';
+        case 9:
+            month = 'Sep';
+        case 10:
+            month = 'Oct';
+        case 11:
+            month = 'Nov';
+        case 12:
+            month = 'Dec';
+    }
 
+    const [animals, setAnimals] = useState([]);
+
+    useEffect(() => {
+        async function fetchAnimals() {
+            try {
+                const res = await fetch("/api/admin/animals");
+                if (!res.ok) {
+                    throw new Error('Failed to fetch animals');
+                }
+                const data = await res.json();
+                const specificAnimal = data.filter(an => {
+                    if (an._id === animal) {
+                        return an;
+                    }
+                })
+                console.log(specificAnimal);
+                setAnimals(specificAnimal);
+            } catch (error) {
+                console.error('Error fetching animals:', error);
+            }
+        }
+        if (userId !== -1) {
+            fetchAnimals();
+        }
+    }, [userId]);
+
+    let breed;
+    let name;
+
+    animals.forEach(animal => {
+        breed = animal.breed;
+        name = animal.name;
+    })
+    
     return (
-        <div className="all">
-            <div id="threeSides">
-                <div id="leftSide">
-                    <p id="date">{components.day}</p>
-                    <p id="monthYear">{components.month} - {components.year}</p>
+        <div className={styles.all}>
+            <div className={styles.threeSides}>
+                <div className={styles.leftSide}>
+                    <p className={styles.date}>{components.day}</p>
+                    <p className={styles.monthYear}>{month} - {components.year}</p>
                 </div>
-                <div id="allInfo">
-                    <div id="info">
-                        <p id="heading">{title} • {hours}</p>
-                        <p id="bottomInfo">{fullName} - {animal}</p>
+                <div className={styles.allInfo}>
+                    <div className={styles.info}>
+                        <div className={styles.top}>
+                            <p className={styles.heading}>{title}</p>
+                            <p className={styles.dot}> • </p>
+                            <p className={styles.num}>{hours} </p>
+                            <p className={styles.hours}>{hours !== 1 ? (<p> hours</p>) : (<p> hour</p>)}</p>
+                        </div>
+                        <div className={styles.bottom}>{fullName} - {breed} - {name}</div>
                     </div>
-                    <p id="desc">{description}</p>
+                    <p className={styles.desc}>{description}</p>
                 </div>
-                <button id="delete" alt="delete" onClick={(e) => {}} ></button>
             </div>
         </div>
     );
