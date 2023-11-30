01ea4d9ca2968ddd95920c3e2f349d4febbaf1d3
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2023-11-29 23:52:15 -0500

    add admin traininglogs dashboard

--- src/pages/admin/traininglogs.js
+++ src/pages/admin/traininglogs.js
@@ -1,21 +1,66 @@
-import { useRouter } from 'next/router';
-import { useState, useEffect } from "react";
-import { useAuth } from '../../hooks/useAuth'
+import React, { useState, useEffect } from 'react';
+import styles from '../../styles/TrainingDashboard.module.css';
 import Sidebar from '../../components/Sidebar';
-import styles from '@/styles/AdminTL.module.css';
+import style1 from '@/styles/AdminTL.module.css';
 
-export default function traininglogs() {
-    const { userId, fullName, admin, logout, login } = useAuth();
-    const router = useRouter();
-    useEffect(()=>{
-        if (userId === -1 || admin === false) {
-            router.push("/login")
-        }
-    },[userId]);
+export default function TrainingLogs() {
+    const [trainingLogs, setTrainingLogs] = useState([]);
+
+    useEffect(() => {
+        const fetchTrainingLogs = async () => {
+            try {
+                const response = await fetch("/api/admin/training");
+                if (!response.ok) {
+                    throw new Error('Failed to fetch');
+                }
+                const logData = await response.json();
+                setTrainingLogs(logData);
+            } catch (error) {
+                console.error("Error fetching training logs: ", error);
+            }
+        };
+        fetchTrainingLogs();
+    }, []);
+
+    const formatDateComponents = (dateString) => {
+        const date = new Date(dateString);
+        return {
+            day: date.getDate(),
+            month: date.getMonth() + 1,
+            year: date.getFullYear()
+        };
+    };
+
     return (
-        <div className={styles.contents}>
+        <div className={style1.contents}>
             <Sidebar selected="ATL"/>
             <h1>TrainingLogs Admin dashboard</h1>
+        
+        <div className={styles.dashboard}>
+            <h1 className={styles.title}>Training Logs</h1>
+            <div className={styles.logList}>
+                {trainingLogs.length > 0 ? (
+                    trainingLogs.map(log => {
+                        const { day, month, year } = formatDateComponents(log.date);
+                        return (
+                            <div key={log._id} className={styles.logItem}>
+                                <div className={styles.dateContainer}>
+                                    <p className={styles.day}>{day}</p>
+                                    <p className={styles.monthYear}>{`${month} - ${year}`}</p>
+                                </div>
+                                <div className={styles.logDetails}>
+                                    <p className={styles.logTitle}>{log.title} • {log.hours} hours</p>
+                                    <p className={styles.logDescription}>{log.description}</p>
+                                </div>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <div className={styles.noLogs}>No training logs available.</div>
+                )}
+            </div>
+        </div>
+
         </div> 
-    )
+    );
 }
