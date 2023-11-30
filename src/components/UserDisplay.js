c96fed08fb5066378b6127156699211fab18f7e8
Armin <arminmoinian@gmail.com>
2023-11-30 05:13:35 -0500

    css for allusers

--- src/components/UserDisplay.js
+++ src/components/UserDisplay.js
@@ -4,14 +4,16 @@ import styles from '@/styles/UserDisplay.module.css';
 export default function UserDisplay({ data }) {
     return (
         <div className={styles.card}>
-            <div className={styles.user_pfp}>
-                <b className={styles.first_letter}>{data.fullName?.charAt(0).toUpperCase()}</b>
-            </div>
             <div className={styles.user_info}>
-                <p className={styles.user_name}><b>{data.fullName}</b></p>
-                <p className={data.admin ? styles.user_identifier : ''}>
-                    {data.admin ? 'Admin' : 'User'}
-                </p>
+                <div className={styles.user_pfp}>
+                    <b>{data.fullName?.charAt(0).toUpperCase()}</b>
+                </div>
+                <div className={styles.user_info_content}>
+                    <p className={styles.user_name}><b>{data.fullName}</b></p>
+                    <p className={data.admin ? styles.user_identifier : ''}>
+                        {data.admin ? 'Admin' : 'User'}
+                    </p>
+                </div>
             </div>
         </div>
     );
