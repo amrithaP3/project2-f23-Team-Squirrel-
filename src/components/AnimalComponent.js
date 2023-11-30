d0862a2dfa0c07a94162fbf60b03bce9a497e89b
shanmathig <shanmathi7215@gmail.com>
2023-11-30 11:52:51 -0500

    format aanimals part 1

--- src/components/AnimalComponent.js
+++ src/components/AnimalComponent.js
@@ -1,28 +1,42 @@
-import { useAuth } from '@/hooks/useAuth'
+// Animal.js
+
+import { useAuth } from '@/hooks/useAuth';
 import styles from '@/styles/Animal.module.css';
 
-import css from 'styled-jsx/css';
 export default function Animal(props) {
-    const { userId, admin, fullName, login, logout } = useAuth();
-    const { animal } = props;
-    // const animal = {
-    //     "name": "Lucy",
-    //     "breed": "Golden Retriever",
-    //     "hoursTrained": 100,
-    //     "profilePicture": "https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-border-collie-play-mn-1100-82d2f1.jpg"
-    // };   
-    return (
-        <div className={styles.animal}>
-             <div className={"dog_name_letter"}>
-                    <p className={"first_letter"}>{animal.name.charAt(0).toUpperCase()}</p>
-                </div>
-            <img className={"doggie"} src={animal.profilePicture} width="350" height="250"></img>
-            <div className={"info"}>
-                <div className="animal_info_contents">
-                    <div className={"animalNameInfo"}>{animal.name} - {animal.breed}</div>
-                    <div>{fullName} - Trained: {animal.hoursTrained} hours</div>
-                </div>
-            </div>
+  const { userId, admin, fullName, login, logout } = useAuth();
+  const { animal } = props;
+
+  return (
+    <>
+      <style jsx>{`
+        .animal {
+          background-color: white;
+          color: black;
+          /* Add any additional styling you need */
+        }
+
+        .dog_name_letter,
+        .doggie,
+        .animal_info_contents {
+          color: black; /* Set the text color to black */
+        }
+
+        /* Add any other styles you need for specific elements */
+      `}</style>
+
+      <div className={`${styles.animal} animal`}>
+        <div className="dog_name_letter">
+          <p className="first_letter">{animal.name.charAt(0).toUpperCase()}</p>
         </div>
-    )
+        <img className="doggie" src={animal.profilePicture} width="350" height="250" />
+        <div className="info">
+          <div className="animal_info_contents">
+            <div className="animalNameInfo">{animal.name} - {animal.breed}</div>
+            <div>{fullName} - Trained: {animal.hoursTrained} hours</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
