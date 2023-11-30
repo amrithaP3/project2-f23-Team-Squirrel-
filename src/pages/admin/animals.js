98962c3af1ea9c347d83152ba261e974c0b7a5e1
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2023-11-29 22:53:53 -0500

    add all animals

--- src/pages/admin/animals.js
+++ src/pages/admin/animals.js
@@ -1,20 +1,39 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from "react";
 import { useAuth } from '../../hooks/useAuth'
-import Sidebar from '../../components/Sidebar';
 
-export default function animals() {
-    const { userId, fullName, admin, logout, login } = useAuth();
-    const router = useRouter();
-    useEffect(()=>{
-        if (userId === -1 || admin === false) {
-            router.push("/login")
-        }
-    },[userId]);
+export default function AnimalsDashboard() {
+    const [animals, setAnimals] = useState([]);
+
+    useEffect(() => {
+        const fetchAnimals = async () => {
+            const result = await fetch("/api/admin/animals"); // Adjust the API endpoint as needed
+            const animalData = await result.json();
+            setAnimals(animalData);
+        };
+
+        fetchAnimals();
+    }, []);
+
     return (
-        <>
-            <Sidebar selected="AA"/>
-            <h1>Animal Admin dashboard</h1>
-        </> 
-    )
+        <div>
+            <h1>Animals</h1>
+            <div>
+                {animals.map(animal => (
+                    <div key={animal._id} className="animal">
+                        <div className="dog_name_letter">
+                            <p className="first_letter">{animal.name.charAt(0).toUpperCase()}</p>
+                        </div>
+                        <img className="doggie" src={animal.profilePicture} alt={animal.name} width="350" height="250" />
+                        <div className="info">
+                            <div className="animal_info_contents">
+                                <div className="animalNameInfo">{animal.name} - {animal.breed}</div>
+                                <div>Trained: {animal.hoursTrained} hours</div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
